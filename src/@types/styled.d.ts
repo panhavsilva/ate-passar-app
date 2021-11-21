41724d34419d58c68ca950fadc2b0e95a7af166d
pamela vieira da silva <panhavsilva@gmail.com>
2021-11-21 16:25:50 -0300

    add resources/theme

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,20 @@
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      primary: string,
+      secondary: string,
+      gradient: string,
+      gradientSecondary: string,
+      gray: string,
+      lightGray: string,
+      purple: string,
+      lightPurple: string,
+      darkPurple: string,
+      red: string,
+      green: string,
+      white: string,
+    },
+  }
+}
+
+export {}
---
+++ src/resources/theme.ts
@@ -0,0 +1,18 @@
+import { DefaultTheme } from 'styled-components'
+
+export const theme: DefaultTheme = {
+  colors: {
+    primary: '#FD749B',
+    secondary: '#281AC8',
+    gradient: 'linear-gradient(to bottom, #fd749b, #e74c9e, #c624a9, #9207b8, #281ac8)',
+    gradientSecondary: 'linear-gradient(to bottom, #fc58b3, #ff6695, #ff7d7c, #ff966c, #feae68)',
+    gray: '#858585',
+    lightGray: '#F0F0F0',
+    purple: '#B327F5',
+    lightPurple: '#F3DBF9',
+    darkPurple: '#6F1265',
+    red: '#FF4949',
+    green: '#5DF888',
+    white: '#FFF9FE',
+  },
+}
