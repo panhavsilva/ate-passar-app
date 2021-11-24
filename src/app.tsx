4627a037a6d516e3eb9a832b52de1f6126ece7fc
pamela vieira da silva <panhavsilva@gmail.com>
2021-11-24 19:22:38 -0300

    add ui icons

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,7 @@
-import styled from 'styled-components/macro'
-import { Nav } from './nav'
-import { Footer } from './footer'
-
 export function App () {
   return (
-    <Container>
-      <Nav />
-      <Footer />
-    </Container>
+    <>
+
+    </>
   )
 }
-
-const Container = styled.div`
-  display: grid;
-  grid-template-areas: 'nav nav' 'sidenav content' 'footer footer';
-  grid-template-columns: 290px 1fr;
-  grid-template-rows: 83px 1fr 70px;
-`
